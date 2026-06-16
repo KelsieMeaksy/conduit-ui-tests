2c3be07cecaed105c386c3058b98446ee24fdf53
Kelsie <kelsieblue2000@gmail.com>
2026-06-16 13:24:38 +0100

    Enhance home page visibility tests
    Expanded home page tests to include visibility checks for additional elements.

--- tests/home-page.spec.ts
+++ tests/home-page.spec.ts
@@ -2,10 +2,20 @@ import {expect, test} from '@playwright/test';
 import {Navigation} from '../POM/navigation';
 
 test.beforeEach(async ({page}) => {
-    page.goto('https://conduit.bondaracademy.com/');
+    await page.goto('https://conduit.bondaracademy.com/');
 });
 
-test('Verify that the Conduit icon is visible on the home page', async ({page}) => {
+test('Verify that all expected elements are visible on the home page', async ({page}) => {
     const navigation = new Navigation(page);
     await expect(navigation.ConduitIcon).toBeVisible();
+    await expect(navigation.HomeButton).toBeVisible();
+    await expect(navigation.SignInButton).toBeVisible();
+    await expect(navigation.SignUpButton).toBeVisible();
+    await expect(navigation.GlobalFeed).toBeVisible();
+    await expect(navigation.ArticleName).toBeVisible();
+    await expect(navigation.ArticleUserIcon).toBeVisible();
+    await expect(navigation.ArticleDate).toBeVisible();
+    await expect(navigation.ArticleFavouriteIcon).toBeVisible();    
+    await expect(navigation.yourFeed).toBeVisible();
 });
+
