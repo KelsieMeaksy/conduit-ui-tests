d707aea2966a4865cc36b72d4129607ab5e91bbf
Kelsie <kelsieblue2000@gmail.com>
2026-06-17 12:54:34 +0100

    Enhance Navigation class with article properties
    Added new properties for article details and follow button.

--- POM/navigation.ts
+++ POM/navigation.ts
@@ -9,8 +9,8 @@ readonly homeButton: Locator;
 readonly signInButton: Locator;
 readonly signUpButton: Locator;
 readonly globalFeed: Locator;
-readonly articleUserIcon: Locator;
-readonly articleName: Locator;
+readonly homeBannerText: Locator;
+readonly articleUsername: Locator;
 readonly articleDate: Locator;
 readonly articleFavouriteIcon: Locator;
 readonly yourFeed: Locator;
@@ -20,6 +20,9 @@ readonly popularTags: Locator;
 readonly conduitBannerText: Locator;
 readonly bannerText: Locator;
 readonly bannerLink: Locator;
+//Article Page
+readonly articleBanner: Locator;
+readonly articleFollowButton: Locator;
 
 
 constructor(page:Page) {
@@ -39,12 +42,18 @@ this.yourFeed = page.getByText('Your Feed');
 this.globalFeed = page.getByText('Global Feed');
 this.popularTags = page.locator('.sidebar', {hasText: 'Popular Tags'});
 // Articles
-this.articleUserIcon = page.getByText('A place to learn and practice test automation.');
-this.articleName = page.locator('.author').first();
+this.homeBannerText = page.getByText('A place to learn and practice test automation.');
+this.articleUsername = page.locator('.author').first();
 this.articleDate = page.locator('.date').first();
 this.articleFavouriteIcon = page.getByRole('button', { name: /\d+/ }).first();
 this.articlePreview = page.locator('.preview-link').first();
 this.readMore = page.getByText('Read more...').first();
+this.articleBanner = page.locator('.banner');
+this.articleFollowButton = page.getByRole('button', { name: /Follow/i}).first();
 
 };
 };
+
+
+//Learn to scrape values from the page and use them in your tests. 
+//For example, you can scrape the article name and date, and then use them to verify that the article is displayed correctly in the feed.
